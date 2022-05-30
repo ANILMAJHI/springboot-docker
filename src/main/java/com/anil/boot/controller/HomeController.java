62685fbf21845609252d4e0fa1a34a73608a4e1b
AnilMajhi63 <anil.majhi63@gmail.com>
2022-05-30 21:21:46 +0530

    first commit

---
+++ src/main/java/com/anil/boot/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.anil.boot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	@GetMapping("/view")
+	public String viewPage()
+	{
+		return "view Page here...";
+	}
+}
